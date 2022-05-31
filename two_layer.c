fb212871e28175852cfd825745dc39e0f3157704
Hyungtak Lee <28820367+hyungtaklee@users.noreply.github.com>
2022-05-31 11:36:36 +0900

    Add two_layer_decoder()

--- two_layer.c
+++ two_layer.c
@@ -1,5 +1,4 @@
 #include "two_layer.h"
-
 #include "crc.h"
 #include "hamming.h"
 
@@ -34,8 +33,44 @@ uint64_t two_layer_encoder(uint16_t data)
         return encoded_data;
 }
 
-bool two_layer_decoder(uint64_t transmitted, uint16_t *data)
+bool two_layer_decoder(uint64_t transmitted, uint16_t *decoded_data)
 {
+        uint8_t transmitted_block;
+        uint8_t syndrome;
+        uint8_t hamming_decoded;
+        uint32_t data_with_crc;
+        uint32_t crc_remainder;
+        int i;
+        
+        if (decoded_data == NULL) {
+                printf("Given address is invalid\n");
+                return false;
+        }
 
-        return false;
+        for (i = 0; i < 6; ++i) {
+                /* get a 7-bit block of transmitted data */
+                transmitted_block = (transmitted & ((uint64_t)0x7F << (5 - i) * 7)) >> ((5 - i) * 7);
+
+                /* decode and fix a part of transmitted data with hamming code */
+                syndrome = hamming_decoder(transmitted_block);
+                hamming_decoded = hamming_error_correction(transmitted_block, syndrome, false);
+
+                /* get a real data in the decoded block and add it */
+                data_with_crc = (data_with_crc << 4) + ((hamming_decoded & 0x78) >> 3);
+        }
+
+        /* decode with CRC-8 */
+        crc_remainder = crc8_decoder(data_with_crc, 24);
+
+        /* If crc_remainder is zero */
+        if (!crc_remainder) {
+                /* remove FCS from the decoded block */
+                *decoded_data = (uint16_t)((data_with_crc >> 8) & 0xFFFF);
+
+                return true;
+        }
+        /* Or remainder is not zero (i.e. error) */
+        else {
+                return false;
+        }
 }
