8d974d9d5bf9c800ab4e387d576060ed5695c2d0
Hyungtak Lee <28820367+hyungtaklee@users.noreply.github.com>
2022-06-02 13:24:03 +0900

    Fix vague operation precedence

--- crc.c
+++ crc.c
@@ -11,7 +11,7 @@ uint32_t mod2_div(uint32_t dividend, int dividend_size)
         int i;
         int iter = dividend_size - DIVISOR_SIZE; /* 24 - 9 */
         const uint32_t top_bit_mask = (1 << (DIVISOR_SIZE - 1)); /* 1 0000 0000 */
-        const uint32_t remainder_mask = ((1 << DIVISOR_SIZE - 1) - 1); /* 1111 1111 */
+        const uint32_t remainder_mask = ((1 << (DIVISOR_SIZE - 1)) - 1); /* 1111 1111 */
 
         remainder = (dividend >> (iter + 1));
         for (i = iter; i >= 0; --i) {
